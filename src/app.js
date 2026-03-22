fae9dfa83f7ea55b4c9ff4e0f9ef687c02a35b33
Sagar Dhankar <sag9315@gmail.com>
2026-03-22 20:19:57 +0530

    Middleware Intro

--- src/app.js
+++ src/app.js
@@ -1,26 +1,30 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get("/admin", function(req,res,next) {
-    const token = "xyz"
-    const isAdminAuthorized = token === "xyzsdjfh"
-    if(isAdminAuthorized) next() 
-    else res.status(401).send("Not authorized")
-})
+//Middleware for /admin/*
+app.use("/admin", function (req, res, next) {
+  console.log("Admin Auth is checked");
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyzdshjfs";
+  if (isAdminAuthorized) {
+    console.log("Admin is authorized");
+    next();
+  } else res.status(401).send("Not authorized");
+});
 
-app.get("/admin/getAllData", function(req, res) {
-    res.send("All data is sent")
-})
+app.get("/admin/getAllData", function (req, res) {
+  res.send("All data is sent");
+});
 
-app.get("/admin/deleteUser", function(req, res) {
-    res.send("Deleted a user")
-})
+app.get("/admin/deleteUser", function (req, res) {
+  res.send("Deleted a user");
+});
 
-app.use("/",function(req,res) {
-    res.send("Hello Hello Hello.")
-})
+app.use("/", function (req, res) {
+  res.send("Hello Hello Hello.");
+});
 
-app.listen(4444,()=>{
-    console.log("Server listening on port 4444 ...")
-})
+app.listen(4444, () => {
+  console.log("Server listening on port 4444 ...");
+});
